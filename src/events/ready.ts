47ab45495994e2d0b9c7b4da78c87d6472a39a95
Muratva Stark <karacaymurat74@gmail.com>
2021-04-21 04:04:42 +0300

    .

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,13 +1,19 @@
 import { IEvent } from "my-module";
 import { CONFIG } from "../config";
 import { Core } from "../helpers/Core";
+import { ExperienceService } from "../helpers/ExperienceService";
 
 export default class Event implements IEvent {
     readonly name = "ready";
 
     async execute(client: Core) {
         const guild = client.guilds.cache.get(CONFIG.SYSTEM.GUILD_ID);
-        if (guild) guild.fetchInvites().then((invites) => client.invites.set(guild.id, invites));
+        if (guild) {
+            guild.fetchInvites().then((invites) => client.invites.set(guild.id, invites));
+            guild.members.cache
+                .filter((member) => client.checkStaff(member.id))
+                .forEach((member) => ExperienceService.sync(member));
+        }
 
         console.log(`[${new Date().toLocaleString()}] ${client.user?.tag} is online!`);
     }
